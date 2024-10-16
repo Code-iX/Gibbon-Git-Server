af15962ae0f9fff9d9e906c4d57242b7b06456bc
Matthias Voigt <m.voigt@code-ix.de>
2024-10-16 02:52:01 +0200

    Class for git added/modified - GitCapabilities - GitCmdResult

---
+++ Gibbon.Git.Server/Git/GitCapabilities.cs
@@ -0,0 +1,11 @@
+﻿namespace Gibbon.Git.Server.Git;
+
+public class GitCapabilities
+{
+    public bool SideBand64K { get; set; }
+    public bool ReportStatus { get; set; }
+    public bool ReportStatusV2 { get; set; }
+    public string ObjectFormat { get; set; }
+    public string Agent { get; set; }
+    public List<string> OtherCapabilities { get; } = [];
+}
