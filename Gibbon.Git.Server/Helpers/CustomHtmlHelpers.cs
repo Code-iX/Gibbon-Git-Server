ed37f6b7ec0eebe0c3e84d0865a8c7436b74b48a
Matthias Voigt <m.voigt@code-ix.de>
2024-10-17 11:25:41 +0200

    Added support for local images #51

--- Gibbon.Git.Server/Helpers/CustomHtmlHelpers.cs
+++ Gibbon.Git.Server/Helpers/CustomHtmlHelpers.cs
@@ -1,18 +1,50 @@
 ﻿using Markdig;
+using Markdig.Syntax;
+using Markdig.Syntax.Inlines;
 
 using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Gibbon.Git.Server.Helpers;
 
 public static class CustomHtmlHelpers
 {
-    public static IHtmlContent MarkdownToHtml(this IHtmlHelper helper, string markdownText)
+    public static IHtmlContent MarkdownToHtml(this IHtmlHelper helper, IUrlHelper urlHelper, string markdownText)
     {
-        var pipeline = new MarkdownPipelineBuilder()
-            .Build();
+        if (string.IsNullOrEmpty(markdownText))
+        {
+            return HtmlString.Empty;
+        }
 
-        var html = Markdown.ToHtml(markdownText, pipeline);
+        var values = new RouteValueDictionary(helper.ViewContext.RouteData.Values);
+
+        var pipeline = new MarkdownPipelineBuilder()
+            .UseAdvancedExtensions()
+            .Build();
+                                              
+        var document = Markdown.Parse(markdownText, pipeline);
+
+        foreach (var link in document.Descendants<LinkInline>())
+        {
+            if (!link.IsImage)
+                continue;
+
+            var originalUrl = link.Url?.TrimStart('.', '/');
+
+            if (string.IsNullOrWhiteSpace(originalUrl))
+                continue;
+
+            if (Uri.IsWellFormedUriString(originalUrl, UriKind.Absolute))
+                continue;
+
+            values["path"] = originalUrl;
+
+            link.Url = urlHelper.Action("Raw", "Repositories", values);
+        }
+
+        var html = document.ToHtml(pipeline);
+
         return new HtmlString(html);
     }
 }
