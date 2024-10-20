8f3689af136a554a91fe692415433078eaa5be8c
Matthias Voigt <m.voigt@code-ix.de>
2024-10-21 01:25:01 +0200

    Introduced UserOutputService for output with user settings

---
+++ Gibbon.Git.Server/Services/UserOutputService.cs
@@ -0,0 +1,42 @@
+﻿namespace Gibbon.Git.Server.Services;
+
+public interface IUserOutputService
+{
+    /// <summary>
+    /// <para>Returns the human-readable file size for an arbitrary, 64-bit file size</para>
+    /// <para>The default format is "0.### XB", e.g. "4.2 KB" or "1.434 GB"</para>
+    /// </summary>
+    string GetFileSizeString(long size);
+}
+
+public class UserOutputService : IUserOutputService
+{
+    /// <summary>
+    /// <para>Returns the human-readable file size for an arbitrary, 64-bit file size</para>
+    /// <para>The default format is "0.### XB", e.g. "4.2 KB" or "1.434 GB"</para>
+    /// </summary>
+    public string GetFileSizeString(long size)
+    {
+        if (size == 0)
+        {
+            return "0 B";
+        }
+
+        string[] suffixes = ["B", "kB", "MB", "GB", "TB"];
+        var readable = Math.Abs((double)size);
+        var suffixIndex = 0;
+
+        while (readable >= 1024 && suffixIndex < suffixes.Length - 1)
+        {
+            readable /= 1024;
+            suffixIndex++;
+        }
+
+        return readable switch
+        {
+            >= 100 => $"{readable:0} {suffixes[suffixIndex]}",
+            > 10 => $"{readable:0.#} {suffixes[suffixIndex]}",
+            _ => $"{readable:0.##} {suffixes[suffixIndex]}"
+        };
+    }
+}
