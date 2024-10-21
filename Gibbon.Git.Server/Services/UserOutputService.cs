a58da648a85735d3c450be13f487883c1735bde9
Matthias Voigt <m.voigt@code-ix.de>
2024-10-21 14:40:07 +0200

    Improved UserOutputService

--- Gibbon.Git.Server/Services/UserOutputService.cs
+++ Gibbon.Git.Server/Services/UserOutputService.cs
@@ -13,30 +13,31 @@ public class UserOutputService : IUserOutputService
 {
     /// <summary>
     /// <para>Returns the human-readable file size for an arbitrary, 64-bit file size</para>
-    /// <para>The default format is "0.### XB", e.g. "4.2 KB" or "1.434 GB"</para>
     /// </summary>
     public string GetFileSizeString(long size)
     {
-        if (size == 0)
-        {
-            return "0 B";
-        }
-
-        string[] suffixes = ["B", "kB", "MB", "GB", "TB"];
+        // TB is more than enough for a 64-bit file size
+        string[] suffixes = ["B", "KB", "MB", "GB", "TB"];
         var readable = Math.Abs((double)size);
         var suffixIndex = 0;
 
-        while (readable >= 1024 && suffixIndex < suffixes.Length - 1)
+        while (readable >= 1000 && suffixIndex < suffixes.Length - 1)
         {
+            // even we divide by 1024, we still use 1000 as the threshold
             readable /= 1024;
             suffixIndex++;
         }
 
+        if (suffixIndex == 0)
+        {
+            return $"{readable:F0} {suffixes[suffixIndex]}";
+        }
+
         return readable switch
         {
-            >= 100 => $"{readable:0} {suffixes[suffixIndex]}",
-            > 10 => $"{readable:0.#} {suffixes[suffixIndex]}",
-            _ => $"{readable:0.##} {suffixes[suffixIndex]}"
+            >= 100 => $"{readable:F0} {suffixes[suffixIndex]}",
+            >= 10 => $"{readable:F1} {suffixes[suffixIndex]}",
+            _ => $"{readable:F2} {suffixes[suffixIndex]}"
         };
     }
 }
