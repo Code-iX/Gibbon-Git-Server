bfc6e58f7de054f002504e99843db43d8d2f5747
Matthias Voigt <m.voigt@code-ix.de>
2024-10-21 16:36:14 +0200

    Moved clipboard to vanilla JS

---
+++ Gibbon.Git.Server/wwwroot/js/clipboard.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    initClipboard();
+});
+
+function initClipboard() {
+    document.body.addEventListener('click', function (event) {
+        const button = event.target.closest('[data-clipboard-text]');
+        if (button) {
+            const textToCopy = button.getAttribute('data-clipboard-text');
+            navigator.clipboard.writeText(textToCopy)
+                .then(() => {
+                    // Success
+                })
+                .catch(err => {
+                    console.error('Failed to copy!', err);
+                });
+        }
+    });
+}
